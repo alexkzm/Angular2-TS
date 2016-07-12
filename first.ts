8f397564ca8e189886ff312953a59c2559b6f903
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-12 15:32:54 -0400

    more ts

--- first.ts
+++ first.ts
@@ -11,3 +11,38 @@ var person: {name: string; age: number;} = {
 	name: name,
 	age: age
 }
+
+function say (name: string): string {
+	return 'Hello' + name
+}
+
+var say = function(name: string): string {
+	return 'Hello' + name
+}
+
+interface aPerson {
+	name: string
+	age: number
+	hunger?: boolean
+}
+
+interface somePerson: aPerson {
+	name: name,
+	age: age,
+	hunger: true
+}
+
+interface aFunction {
+	(source: string, subString: string): boolean;
+}
+
+var func: aFunction
+func = function(source: string, subString: string) {
+	var result source.search(subString)
+	if(result == -1) {
+		return false
+	}
+	else {
+		return true
+	}
+}
