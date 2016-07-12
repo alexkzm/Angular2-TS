d1ea718c3ec698d8eb7429e7e8ac518dc34ab43c
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-11 22:13:17 -0400

    tststs

---
+++ first.ts
@@ -0,0 +1,13 @@
+var name: string = 'Shurik'
+var age: number = 18
+var happy: boolean = true
+var anything: any = 2016
+anything = 'this year'
+
+var array: string[] = ['one', 'two', 'three'] 
+var favorite: Array<number> = [7,13,15]
+
+var person: {name: string; age: number;} = {
+	name: name,
+	age: age
+}
