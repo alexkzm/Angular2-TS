050240610dfddf406953579f68f1d92978878db8
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-21 17:26:57 -0400

    building

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {boostrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+import {Component, provide, enableProdMode, Inject} from 'angular2/core'
+
+enableProdMode()
+boostrap(AppComponent)
