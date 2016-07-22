1e8cf10cbf39fa16cff467e16f4d80c2d3cd5653
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-22 14:08:44 -0400

    observing

--- app/state.ts
+++ app/state.ts
@@ -13,4 +13,19 @@ export class RxWebSocket {
 	didOpen: (e:Event) => void
 	willOpen: () => void
 	didClose: (e?:any) => void
+
+	selector(e: MessageEvent) {
+		return JSON.parse(e.data)
+	}
+
+	constructor(private url: string, private WebSocketCtor: {new(url:string): WebSocket} = WebSocket){
+	}
+
+	static create(url:string, WebSocketCtor: new(url:string): WebSocket = WebSocket): RxWebSocket {
+	return new RxWebSocket(url, WebSocketCtor)
+	}
+
+	_out: Observable<any>
+	_in: Observer<any>
 }
+
