d7aa4e60b9330420394e4e467badc7f1ca07fe70
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-23 21:04:47 -0400

    servicing

--- app/center.ts
+++ app/center.ts
@@ -11,6 +11,38 @@ export enum ConnectionState {
 export interface TicketMessage {
 	symbol: string,
 	price: number,
-	timestamp:number
+	timestamp: number
+}
+
+@Injectable()
+export class TickService {
+	connectionState = new BehaviorSubject<ConnectionState>(ConnectionState.CONNECTING)
+
+	constructor(@Inject(SOCKET_URL) url: string, public socket:RxWebSocket) {
+	const connectionState = this.connectionState
+
+	socket.didOpen = () => {
+	  connectionState.next(ConnectionState.CONNECTED)
+	}
+
+	socket.willOpen = () => {
+	  connectionState.next(ConnectionState.CONNECTING)
+	}
+
+	socket.willOpen = () => {
+	  connectionState.next(ConnectionState.CLOSED)
+	}
+	}
+
+	getTicker(symbol: string): Observable<TickMessage> {
+	  const socket = this.socket
+	  return Observable.create(subscriber => {
+	  	const msgSub = socket.out
+	  	.filter(d => d.symbol === symbol)
+	  	.subscribe(subscriber)
+	  	socket.send({symbol, type: 'sub'})
+	  	
+	  })
+	}
 }
 
